c9f1467aa8fb25ec2ebe4d4bba97acfb7bab92ee
Gyeonghun Min <riverpower6@naver.com>
2025-12-29 16:58:13 +0900

    [TEST][BE] 다건조회 TC 추가

---
+++ backend/src/test/kotlin/io/river/backend/domain/post/post/controller/ApiV1PostControllerTest.kt
@@ -0,0 +1,53 @@
+package io.river.backend.domain.post.post.controller
+
+import io.river.backend.domain.post.post.service.PostService
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class ApiV1PostControllerTest {
+
+    @Autowired
+    private lateinit var postService: PostService
+
+    @Autowired
+    private lateinit var mvc: MockMvc
+
+    @Test
+    @DisplayName("다건 조회")
+    fun t1() {
+        val resultActions = mvc
+            .perform(
+                MockMvcRequestBuilders.get("/api/v1/posts")
+            )
+            .andDo(MockMvcResultHandlers.print())
+
+        val posts = postService.findAll()
+
+        resultActions
+            .andExpect(handler().handlerType(ApiV1PostController::class.java))
+            .andExpect(handler().methodName("getItems"))
+            .andExpect(status().isOk())
+
+        for (i in posts.indices) {
+            val post = posts[i]
+
+            resultActions
+                .andExpect(jsonPath("$[$i].id").value(post.id))
+                .andExpect(jsonPath("$[$i].title").value(post.title))
+                .andExpect(jsonPath("$[$i].content").value(post.content))
+        }
+    }
+}
