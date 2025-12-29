dad4b0d03295ea6e2f53327854dded588f869f98
Gyeonghun Min <riverpower6@naver.com>
2025-12-29 17:25:51 +0900

    [CHORE][FE] 개발모드/운영모드에 따른 URL 등 프로퍼티 및 App 설정

--- frontend/src/app/ClientPage.tsx
+++ frontend/src/app/ClientPage.tsx
@@ -6,7 +6,9 @@ export default function ClientPage() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/v1/posts")
+    const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+    fetch(`${API_BASE_URL}/api/v1/posts`)
       .then((res) => res.json())
       .then((data) => setPosts(data));
   }, []);
