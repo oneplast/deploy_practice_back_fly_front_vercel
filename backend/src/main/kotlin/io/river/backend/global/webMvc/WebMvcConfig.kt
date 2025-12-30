bd471c0dbe463c8553f2ddf94617f3437622e78c
GyeongHun <riverpower6@naver.com>
2025-12-30 14:05:02 +0900

    [CHORE][BE] resourceLocation을 보다 안전한 방법으로 변경
    - 기존 방법은 file:///을 이용해 location 지정
    - Paths.get().toUri.toString() 방식으로 변경
      - 윈도우/리눅스 어떤 환경에서든 location이 제대로 설정되어 적용

--- backend/src/main/kotlin/io/river/backend/global/webMvc/WebMvcConfig.kt
+++ backend/src/main/kotlin/io/river/backend/global/webMvc/WebMvcConfig.kt
@@ -4,12 +4,15 @@ import io.river.backend.global.app.AppConfig
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.nio.file.Paths
 
 @Configuration
 class WebMvcConfig : WebMvcConfigurer {
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        val location = Paths.get(AppConfig.getGenFileDirPath()).toUri().toString()
+
         registry.addResourceHandler("/gen/**")
-            .addResourceLocations("file:///" + AppConfig.getGenFileDirPath() + "/")
+            .addResourceLocations(location)
     }
 }
